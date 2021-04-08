18f58c2243de0919acd7d3ffdb5a0c91d0d105df
Rahil <hackerinsiderahil@gmail.com>
2021-04-08 16:57:35 +0530

    Increased Security & limits

--- OTP_Verification/src/main/java/api/verification/library/InvalidServerException.java
+++ OTP_Verification/src/main/java/api/verification/library/InvalidServerException.java
@@ -1,5 +1,11 @@
 package api.verification.library;
 
+import android.security.identity.InvalidRequestMessageException;
+
+import com.sun.mail.smtp.SMTPAddressFailedException;
+
+import javax.mail.internet.AddressException;
+
 public class InvalidServerException extends RuntimeException{
     public InvalidServerException(String s){
         super(s);
@@ -7,6 +13,12 @@ public class InvalidServerException extends RuntimeException{
 }
 class NullOTPException extends NullPointerException{
     public NullOTPException(String s){
+        super(s);
+    }
+}
+
+class DisposableEmailException extends AddressException {
+    public DisposableEmailException(String s){
         super(s);
     }
 }
